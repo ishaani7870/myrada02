da44006c20f313e373708888af70fcdc190dee71
kajalishaani <kajalishaani21@navgurukul.org>
2024-11-26 15:14:48 +0530

    retun the app name

--- fe/index.js
+++ fe/index.js
@@ -1,12 +1,14 @@
 function load_officers(){
 	$.getScript('/fe/pages/officer/officer_list.js')
 }
-load_officers();
-	
+
 
 function load_donors(){
 	$.getScript('/fe/pages/donor/donor_list.js')
 }
 
-load_donors();
+load_officers();
+	
+
+
 
