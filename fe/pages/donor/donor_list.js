ebe8bd8348337cddf72dcd322e169b08f169cc4b
kajalishaani <kajalishaani21@navgurukul.org>
2024-11-26 15:09:36 +0530

    doner page retun

---
+++ fe/pages/donor/donor_list.js
@@ -0,0 +1,9 @@
+function init_donor_list(){
+	$('#x-body').load('/fe/pages/donor/donor_list.htm')
+	// $.get( "/api/author/", function( data ) {
+		// render_author_table(data);
+	// });
+}
+
+
+init_donor_list();
